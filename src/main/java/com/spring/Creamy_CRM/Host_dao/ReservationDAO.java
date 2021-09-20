cc455dd324c8cfc4f13f0f37d28f3a8d07861e88
sky2000sh <sky2000sh@gmail.com>
2021-09-21 00:39:25 +0900

    [하기 관리자 예약 파일 입력 및 수정] . ReservationService . ReservationServiceImpl . ReservationDAO . ReservationDAOImpl . reservationVO

--- src/main/java/com/spring/Creamy_CRM/Host_dao/ReservationDAO.java
+++ src/main/java/com/spring/Creamy_CRM/Host_dao/ReservationDAO.java
@@ -1,11 +1,44 @@
 /*
- * 작성자 : 
- * 작성일 : 
+ * 작성자 : 이시현
+ * 작성일 : 2021-09-20 ~ 2021-09-20
  * 예약 관련 dao
  * 
 */
 package com.spring.Creamy_CRM.Host_dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.spring.Creamy_CRM.VO.reservationVO;
+
+
 public interface ReservationDAO {
 
+//======= 예약요청 탭 =======	
+	// 예약요청 목록 갯수 구하기
+	public int getRequestCnt();
+	
+	// 예약요청 목록 조회
+	public List<reservationVO> getRequestList(Map<String, Object> map);
+	
+	// 예약요청 상세 페이지, 수정 상세 페이지
+	public reservationVO getRequestDetail(int num);
+	
+	// 예약요청 수정 처리 페이지
+	public int updateRequest(reservationVO vo);
+	
+	// 예약요청 삭제 처리 페이지
+	public int deleteRequest(int num);
+	
+//======= 예약조회 탭 =======
+	// 예약조회 목록 갯수 구하기
+	public int getCompleteCnt();
+	
+	// 예약요청 목록 조회
+	public List<reservationVO> getCompleteList(Map<String, Object> map);
+	
+	// 예약조회 상세 페이지, 수정 상세 페이지
+	//public void reservationVO getCompleteDetail(int num);
+		
+	
 }
