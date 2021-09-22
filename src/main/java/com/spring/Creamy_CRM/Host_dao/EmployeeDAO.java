b0b0a727841a803e6cf0d3cb4bf4873ba7379533
sioux77 <yebien@gmail.com>
2021-09-22 13:24:25 +0900

    1) employee_enlist - Auth_tbl과 employee_tbl에 insert - 중복확인 등 처리를 위한 employee_enlist.jsp 수정 - EmployeeService와 EmployeeDAO 수정 - employee_sqlMapper 수정
    2) 근태 일부 수정
    - employee_attendance service 작성 중

--- src/main/java/com/spring/Creamy_CRM/Host_dao/EmployeeDAO.java
+++ src/main/java/com/spring/Creamy_CRM/Host_dao/EmployeeDAO.java
@@ -7,6 +7,7 @@
 package com.spring.Creamy_CRM.Host_dao;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 import com.spring.Creamy_CRM.VO.EmployeeVO;
 
@@ -15,7 +16,18 @@ public interface EmployeeDAO {
 	// 직원 목록 조회
 	public ArrayList<EmployeeVO> employeeList(String host_code);
 	
+	// 이미 직원 등록이 완료된 id인지 체크
+	public int chkEmployeeIdPreexisting(String employee_id);
+	
+	// 직원 등록시 해당 id가 있는지 체크 
+	public int chkEmployeeIdAction(String employee_id);
+	
+	// 직원 id와 pwd를 Auth 테이블에 삽입 
+	public int insertEmployeeAuth(Map<String, Object> map);
+	
 	// 직원 등록 
 	public int insertEmployee(EmployeeVO vo);
+	
+	
 	
 }
