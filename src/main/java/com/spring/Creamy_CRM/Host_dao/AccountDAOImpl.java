c34b6dad6bd500044f9108e5a4b3ac0599bc37d7
sioux77 <yebien@gmail.com>
2021-09-27 00:34:57 +0900

    Account - 회계 판관비

--- src/main/java/com/spring/Creamy_CRM/Host_dao/AccountDAOImpl.java
+++ src/main/java/com/spring/Creamy_CRM/Host_dao/AccountDAOImpl.java
@@ -6,9 +6,34 @@
 */
 package com.spring.Creamy_CRM.Host_dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.spring.Creamy_CRM.VO.AccountVO;
 
 @Repository
 public class AccountDAOImpl implements AccountDAO {
+	
+	@Autowired
+	SqlSession sqlSessoin;
+	
+	// 매입매출 전표 목록
+	@Override
+	public List<AccountVO> getSlipList(String host_code) {
+		AccountDAO dao = sqlSessoin.getMapper(AccountDAO.class);
+		return dao.getSlipList(host_code);
+	}
+	
+	// 매입매출 전표 등록
+	@Override
+	public int insertSlip(AccountVO vo) {
+		AccountDAO dao = sqlSessoin.getMapper(AccountDAO.class);
+		return dao.insertSlip(vo);
+	}
+
+
 
 }
--- src/main/java/com/spring/Creamy_CRM/Host_service/AccountService.java
+++ src/main/java/com/spring/Creamy_CRM/Host_service/AccountService.java
@@ -6,6 +6,14 @@
 */
 package com.spring.Creamy_CRM.Host_service;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.ui.Model;
+
 public interface AccountService {
+	
+	// 판관비 목록
+	public void slipList(HttpServletRequest req, Model model);
+	
 
 }
